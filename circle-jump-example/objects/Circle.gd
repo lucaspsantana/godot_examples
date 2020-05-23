63b73f50e88f348fc2287f2ccf0f56e05a781f77
Lucas Santana <lanatnas36@gmail.com>
2020-05-22 23:50:29 -0300

    Main.gd e animation

--- circle-jump-example/objects/Circle.gd
+++ circle-jump-example/objects/Circle.gd
@@ -2,18 +2,26 @@ extends Area2D
 
 onready var orbit_position = $Pivot/OrbitPosition
 var radius = 100
-var rotation_speed = PI
+var rotation_speed = PI  #velocidade de rotação 
 
-func _ready():
-	init()
-
-func init(_radius=radius):
+func init(_position, _radius=radius):
+	position = _position
 	radius = _radius
 	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
 	$CollisionShape2D.shape.radius = radius
 	var img_size = $Sprite.texture.get_size().x / 2
 	$Sprite.scale = Vector2(1, 1) * radius / img_size
 	orbit_position.position.x = radius + 25
+	rotation_speed *= pow(-1, randi() % 2)
 
-func _process(delta):
+func _process(delta):  #rotação do pivê em torno do circulo
 	$Pivot.rotation += rotation_speed * delta
+	
+func implode():
+	$AnimationPlayer.play("implode")
+	yield($AnimationPlayer, "animation_finished")
+	queue_free()
+
+func capture():
+	$AnimationPlayer.play("capture")
+
--- circle-jump-example/objects/Jumper.gd
+++ circle-jump-example/objects/Jumper.gd
@@ -1,23 +1,35 @@
 extends Area2D
 
+signal captured
+
+onready var trail = $Trail/Points
+
 var velocity = Vector2(100,0) #valor inicial para teste
 var jump_speed = 1000
 var target = null  #Se estamos no circulo 
+var trail_length = 25
 
 func _unhandled_input(event): #funçao para detectar toque na tela
 	if target and event is InputEventScreenTouch and event.pressed: #se estiver no circulo
 		jump() # função para saltar
 
 func jump(): #função de salto
+	target.implode()
 	target = null    #sair do circulo  
-	velocity = transform.x * jump_speed #avançar a vel de salto
+	velocity = transform.x * jump_speed #avançar o Jumper
 
 func _on_Jumper_area_entered(area): #detecta um circulo com sinal de area entered
 	target = area  #conecta-se
+	target.get_node("Pivot").rotation = (position - target.position).angle() #começar a rodar no local atingido
 	velocity = Vector2.ZERO #parmaos de avançar
+	emit_signal("captured",area) #sinal será emitido quando o 
+#jumper atingir o ciculo 
 
 func _physics_process(delta):
+	if trail.points.size() > trail_length:
+		trail.remove_point(0)
+	trail.add_point(position)
 	if target: #se acertar o circulo giro em torno dele 
-		transform = target.orbit_position.global_transform #
+		transform = target.orbit_position.global_transform
 	else: #senão, avançamos em linha reta 
 		position += velocity * delta
